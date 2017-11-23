91161f95f05a5538e534c3c67088b5eb5424a828
takahirom <takam.dev@gmail.com>
2017-11-23 12:55:21 +0900

    Move MainActivity to presentation

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/MainActivity.java
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/MainActivity.java
@@ -1,4 +1,4 @@
-package io.github.droidkaigi.confsched2018;
+package io.github.droidkaigi.confsched2018.presentation;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import io.github.droidkaigi.confsched2018.R;
 
 public class MainActivity extends AppCompatActivity {
 
