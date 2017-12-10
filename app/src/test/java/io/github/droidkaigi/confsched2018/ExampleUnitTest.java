c7f2c16cb3ae7bbe9236ba926f0db954aa34b4a3
takahirom <takam.dev@gmail.com>
2017-12-10 19:11:22 +0900

    Fix eof new line format

--- app/src/test/java/io/github/droidkaigi/confsched2018/ExampleUnitTest.java
+++ app/src/test/java/io/github/droidkaigi/confsched2018/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package io.github.droidkaigi.confsched2018;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
