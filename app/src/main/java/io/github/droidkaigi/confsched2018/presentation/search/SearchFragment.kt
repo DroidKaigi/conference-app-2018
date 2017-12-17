0eb40830d804b92cb73b120ff17e72c012936eb5
takahirom <takam.dev@gmail.com>
2017-12-17 14:25:44 +0900

    Add date header view

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/search/SearchFragment.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/search/SearchFragment.kt
@@ -3,8 +3,15 @@ package io.github.droidkaigi.confsched2018.presentation.search
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.constraint.ConstraintSet
+import android.support.transition.Fade
+import android.support.transition.TransitionManager
+import android.support.transition.TransitionSet
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.SCROLL_STATE_IDLE
 import android.support.v7.widget.SearchView
 import android.view.*
 import com.xwray.groupie.GroupAdapter
@@ -15,7 +22,9 @@ import io.github.droidkaigi.confsched2018.di.Injectable
 import io.github.droidkaigi.confsched2018.model.Session
 import io.github.droidkaigi.confsched2018.presentation.Result
 import io.github.droidkaigi.confsched2018.presentation.binding.FragmentDataBindingComponent
+import io.github.droidkaigi.confsched2018.presentation.sessions.item.DateHeaderItem
 import io.github.droidkaigi.confsched2018.presentation.sessions.item.DateSessionsGroup
+import io.github.droidkaigi.confsched2018.presentation.sessions.item.SessionItem
 import io.github.droidkaigi.confsched2018.util.ext.observe
 import timber.log.Timber
 import javax.inject.Inject
@@ -31,7 +40,25 @@ class SearchFragment : Fragment(), Injectable {
         ViewModelProviders.of(this, viewModelFactory).get(SearchViewModel::class.java)
     }
 
+    private val dayVisibleConstraintSet by lazy {
+        ConstraintSet().apply {
+            clone(context, R.layout.fragment_all_sessions)
+            setVisibility(R.id.day, ConstraintSet.VISIBLE)
+        }
+    }
+
+    private val dayInvisibleConstraintSet by lazy {
+        ConstraintSet().apply {
+            clone(context, R.layout.fragment_all_sessions)
+            setVisibility(R.id.day, ConstraintSet.GONE)
+        }
+    }
+
     private val onFavoriteClickListener = { session: Session ->
+        // Just for response
+        session.isFavorited = !session.isFavorited
+        binding.sessionsRecycler.adapter.notifyDataSetChanged()
+
         searchViewModel.onFavoriteClick(session)
     }
 
@@ -54,19 +81,14 @@ class SearchFragment : Fragment(), Injectable {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.searchRecycler.adapter = GroupAdapter<ViewHolder>().apply {
-            add(sessionsGroup)
-            setOnItemClickListener({ item, view ->
-                //TODO
-            })
-        }
+        setupRecyclerView()
 
         searchViewModel.sessions.observe(this, { result ->
             when (result) {
                 is Result.Success -> {
                     val sessions = result.data
                     sessionsGroup.updateSessions(sessions, onFavoriteClickListener)
-                    binding.searchRecycler.scrollToPosition(0)
+                    binding.sessionsRecycler.scrollToPosition(0)
                 }
                 is Result.Failure -> {
                     Timber.e(result.e)
@@ -74,6 +96,59 @@ class SearchFragment : Fragment(), Injectable {
             }
         })
         lifecycle.addObserver(searchViewModel)
+    }
+
+
+    private fun setupRecyclerView() {
+        val groupAdapter = GroupAdapter<ViewHolder>().apply {
+            add(sessionsGroup)
+            setOnItemClickListener({ item, view ->
+                //TODO
+            })
+        }
+        binding.sessionsRecycler.adapter = groupAdapter
+        val linearLayoutManager = binding.sessionsRecycler.layoutManager as LinearLayoutManager
+        binding.sessionsRecycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                val transitionSet = TransitionSet()
+                        .addTransition(Fade(Fade.OUT).setStartDelay(400))
+                        .addTransition(Fade(Fade.IN))
+                        .excludeTarget(binding.sessionsRecycler, true)
+                TransitionManager
+                        .beginDelayedTransition(
+                                binding.sessionsConstraintLayout,
+                                transitionSet)
+                when (newState) {
+                    SCROLL_STATE_IDLE -> {
+                        dayInvisibleConstraintSet.applyTo(binding.sessionsConstraintLayout)
+                    }
+                    else -> {
+                        dayVisibleConstraintSet.applyTo(binding.sessionsConstraintLayout)
+                    }
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val firstItemPosition = linearLayoutManager.findFirstVisibleItemPosition()
+                var item = groupAdapter.getItem(firstItemPosition)
+                item = item as? DateHeaderItem ?: groupAdapter.getItem(firstItemPosition + 1)
+                when (item) {
+                    is SessionItem -> {
+                        val dayTitle = if (item.session.startTime.getDate() == 2) {
+                            getString(R.string.day1)
+                        } else {
+                            getString(R.string.day2)
+                        }
+                        // Prevent requestLayout()
+                        if (dayTitle != binding.day.text) {
+                            binding.day.text = dayTitle
+                        }
+                    }
+                }
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/sessions/AllSessionsFragment.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/sessions/AllSessionsFragment.kt
@@ -3,18 +3,28 @@ package io.github.droidkaigi.confsched2018.presentation.sessions
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.constraint.ConstraintSet
+import android.support.transition.Fade
+import android.support.transition.TransitionManager
+import android.support.transition.TransitionSet
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
+import io.github.droidkaigi.confsched2018.R
 import io.github.droidkaigi.confsched2018.databinding.FragmentAllSessionsBinding
 import io.github.droidkaigi.confsched2018.di.Injectable
 import io.github.droidkaigi.confsched2018.model.Session
 import io.github.droidkaigi.confsched2018.presentation.Result
 import io.github.droidkaigi.confsched2018.presentation.binding.FragmentDataBindingComponent
+import io.github.droidkaigi.confsched2018.presentation.sessions.item.DateHeaderItem
 import io.github.droidkaigi.confsched2018.presentation.sessions.item.DateSessionsGroup
+import io.github.droidkaigi.confsched2018.presentation.sessions.item.SessionItem
 import io.github.droidkaigi.confsched2018.util.ext.observe
 import timber.log.Timber
 import javax.inject.Inject
@@ -31,7 +41,25 @@ class AllSessionsFragment : Fragment(), Injectable {
         ViewModelProviders.of(this, viewModelFactory).get(AllSessionsViewModel::class.java)
     }
 
+    private val dayVisibleConstraintSet by lazy {
+        ConstraintSet().apply {
+            clone(context, R.layout.fragment_all_sessions)
+            setVisibility(R.id.day, ConstraintSet.VISIBLE)
+        }
+    }
+
+    private val dayInvisibleConstraintSet by lazy {
+        ConstraintSet().apply {
+            clone(context, R.layout.fragment_all_sessions)
+            setVisibility(R.id.day, ConstraintSet.GONE)
+        }
+    }
+
     private val onFavoriteClickListener = { session: Session ->
+        // Just for response
+        session.isFavorited = !session.isFavorited
+        binding.sessionsRecycler.adapter.notifyDataSetChanged()
+
         sessionsViewModel.onFavoriteClick(session)
     }
 
@@ -46,12 +74,7 @@ class AllSessionsFragment : Fragment(), Injectable {
         super.onActivityCreated(savedInstanceState)
         lifecycle.addObserver(sessionsViewModel)
 
-        binding.sessionsRecycler.adapter = GroupAdapter<ViewHolder>().apply {
-            add(sessionsGroup)
-            setOnItemClickListener({ item, view ->
-                //TODO
-            })
-        }
+        setupRecyclerView()
 
         sessionsViewModel.sessions.observe(this, { result ->
             when (result) {
@@ -61,6 +84,58 @@ class AllSessionsFragment : Fragment(), Injectable {
                 }
                 is Result.Failure -> {
                     Timber.e(result.e)
+                }
+            }
+        })
+    }
+
+    private fun setupRecyclerView() {
+        val groupAdapter = GroupAdapter<ViewHolder>().apply {
+            add(sessionsGroup)
+            setOnItemClickListener({ item, view ->
+                //TODO
+            })
+        }
+        binding.sessionsRecycler.adapter = groupAdapter
+        val linearLayoutManager = binding.sessionsRecycler.layoutManager as LinearLayoutManager
+        binding.sessionsRecycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                val transitionSet = TransitionSet()
+                        .addTransition(Fade(Fade.OUT).setStartDelay(400))
+                        .addTransition(Fade(Fade.IN))
+                        .excludeTarget(binding.sessionsRecycler, true)
+                TransitionManager
+                        .beginDelayedTransition(
+                                binding.sessionsConstraintLayout,
+                                transitionSet)
+                when (newState) {
+                    AbsListView.OnScrollListener.SCROLL_STATE_IDLE -> {
+                        dayInvisibleConstraintSet.applyTo(binding.sessionsConstraintLayout)
+                    }
+                    else -> {
+                        dayVisibleConstraintSet.applyTo(binding.sessionsConstraintLayout)
+                    }
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val firstItemPosition = linearLayoutManager.findFirstVisibleItemPosition()
+                var item = groupAdapter.getItem(firstItemPosition)
+                item = item as? DateHeaderItem ?: groupAdapter.getItem(firstItemPosition + 1)
+                when (item) {
+                    is SessionItem -> {
+                        val dayTitle = if (item.session.startTime.getDate() == 2) {
+                            getString(R.string.day1)
+                        } else {
+                            getString(R.string.day2)
+                        }
+                        // Prevent requestLayout()
+                        if (dayTitle != binding.day.text) {
+                            binding.day.text = dayTitle
+                        }
+                    }
                 }
             }
         })
