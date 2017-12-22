0b4e3c49e79c0021a6ee52d593ca510849b382e6
takahirom <takam.dev@gmail.com>
2017-12-23 00:00:07 +0900

    Add Index for performance

--- app/src/main/java/io/github/droidkaigi/confsched2018/data/db/entity/SessionSpeakerJoinEntity.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/data/db/entity/SessionSpeakerJoinEntity.kt
@@ -1,5 +1,6 @@
 package io.github.droidkaigi.confsched2018.data.db.entity
 
+import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.ForeignKey
 import android.arch.persistence.room.ForeignKey.CASCADE
@@ -7,14 +8,17 @@ import android.arch.persistence.room.ForeignKey.CASCADE
 @Entity(tableName = "session_speaker_join", primaryKeys = ["sessionId", "speakerId"],
         foreignKeys = [
             (ForeignKey(
-                entity = SessionEntity::class,
-                parentColumns = arrayOf("id"),
-                childColumns = arrayOf("sessionId"),
+                    entity = SessionEntity::class,
+                    parentColumns = arrayOf("id"),
+                    childColumns = arrayOf("sessionId"),
                     onDelete = CASCADE)),
             (ForeignKey(
-                entity = SpeakerEntity::class,
-                parentColumns = arrayOf("id"),
-                childColumns = arrayOf("speakerId"),
-                onDelete = CASCADE))]
+                    entity = SpeakerEntity::class,
+                    parentColumns = arrayOf("id"),
+                    childColumns = arrayOf("speakerId"),
+                    onDelete = CASCADE))]
 )
-class SessionSpeakerJoinEntity(val sessionId: String, val speakerId: String)
+class SessionSpeakerJoinEntity(
+        val sessionId: String,
+        @ColumnInfo(index = true)
+        val speakerId: String)
