e7555cbc863fc3c40f3e17693725089493d491ed
takahirom <takam.dev@gmail.com>
2018-01-08 21:45:48 +0900

    Delete needless line

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/binding/FloatingActionButtonBinding.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/binding/FloatingActionButtonBinding.kt
@@ -6,7 +6,6 @@ import android.graphics.drawable.Drawable
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.graphics.drawable.DrawableCompat
 
-
 @BindingAdapter("tintCompat")
 fun FloatingActionButton.setTintCompat(colorStateList: ColorStateList) {
     var icon: Drawable? = drawable
