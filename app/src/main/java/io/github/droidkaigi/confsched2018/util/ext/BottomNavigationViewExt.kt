8819273b9dfb2545829dc2a7841cd2086a745afa
neonankiti <yk.nanri@gmail.com>
2018-01-13 02:33:14 -0800

    remove the label for BottomNavigationView When shift mode comes to disable, the title should be removed, and icon is centered of BottomNavigationItem

--- app/src/main/java/io/github/droidkaigi/confsched2018/util/ext/BottomNavigationViewExt.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/util/ext/BottomNavigationViewExt.kt
@@ -4,7 +4,10 @@ import android.annotation.SuppressLint
 import android.support.design.internal.BottomNavigationItemView
 import android.support.design.internal.BottomNavigationMenuView
 import android.support.design.widget.BottomNavigationView
-import android.util.Log
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import timber.log.Timber
 
 @SuppressLint("RestrictedApi")
 fun BottomNavigationView.disableShiftMode() {
@@ -14,8 +17,23 @@ fun BottomNavigationView.disableShiftMode() {
         shiftingMode.isAccessible = true
         shiftingMode.setBoolean(menuView, false)
         shiftingMode.isAccessible = false
-        for (i in 0 until menuView.childCount) {
-            val item = menuView.getChildAt(i) as BottomNavigationItemView
+        menuView.eachChildView {
+            val item = it as BottomNavigationItemView
+            // NOTE: This removing the BottomNavigationItem labels including small and large text container.
+            item.removeViewAt(1)
+
+            // set the gravity of item icon to Center.
+            val lp = FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT)
+            lp.gravity = Gravity.CENTER
+            item.getChildAt(0).layoutParams = lp
+
+            // remove the default top margin.
+            val topMargin = item.javaClass.getDeclaredField("mDefaultMargin")
+            topMargin.isAccessible = true
+            topMargin.setInt(item, 0)
+            topMargin.isAccessible = false
 
             item.setShiftingMode(false)
             // set once again checked value, so view will be updated
@@ -23,8 +41,8 @@ fun BottomNavigationView.disableShiftMode() {
             item.setChecked(item.itemData!!.isChecked)
         }
     } catch (e: NoSuchFieldException) {
-        Log.e("BNVHelper", "Unable to get shift mode field", e)
+        Timber.e(e, "Unable to get shift mode field")
     } catch (e: IllegalAccessException) {
-        Log.e("BNVHelper", "Unable to change value of shift mode", e)
+        Timber.e(e, "Unable to change value of shift mode")
     }
 }
