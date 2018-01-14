e824980744acc6a9c97ce45259eca67d370764d3
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2018-01-15 01:21:43 +0900

    Kotshi automatically checks non-null, nullable of response properties

--- app/src/main/java/io/github/droidkaigi/confsched2018/data/api/response/mapper/SessionDataMapperExt.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/data/api/response/mapper/SessionDataMapperExt.kt
@@ -37,7 +37,7 @@ fun Session.toSessionEntity(categories: List<Category>?, rooms: List<Room>?): Se
     val topic = categories.category(2, categoryItems[2])
     val level = categories.category(3, categoryItems[3])
     return SessionEntity(
-            id = id!!,
+            id = id,
             title = title!!,
             desc = description!!,
             stime = startsAt!!,
