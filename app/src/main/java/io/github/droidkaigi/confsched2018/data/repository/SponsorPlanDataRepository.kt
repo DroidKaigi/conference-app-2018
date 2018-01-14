52b6305057dd008b2792a7e698822429acc809e6
Jumpei Matsuda <j.matsuda.bb26th@gmail.com>
2018-01-14 23:30:59 +0900

    Applied a tiny refactoring

--- app/src/main/java/io/github/droidkaigi/confsched2018/data/repository/SponsorPlanDataRepository.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/data/repository/SponsorPlanDataRepository.kt
@@ -5,29 +5,34 @@ import io.github.droidkaigi.confsched2018.model.Sponsor
 import io.github.droidkaigi.confsched2018.model.SponsorGroup
 import io.github.droidkaigi.confsched2018.model.SponsorPlan
 import io.reactivex.Flowable
-import timber.log.Timber
 import javax.inject.Inject
+import io.github.droidkaigi.confsched2018.data.api.response.SponsorPlan.Type as ResponseSponsorType
 
 class SponsorPlanDataRepository @Inject constructor(
         private val sponsorApi: SponsorApi
 ) : SponsorPlanRepository {
     override val sponsorPlans: Flowable<List<SponsorPlan>>
-        get() = sponsorApi.sponsorPlans().toFlowable().map {
+        get() = sponsorApi.sponsorPlans().map {
             it.map {
                 SponsorPlan(it.planName, when (it.planType) {
-                    io.github.droidkaigi.confsched2018.data.api.response.SponsorPlan.Type.PLATINUM -> SponsorPlan.Type.Platinum
-                    io.github.droidkaigi.confsched2018.data.api.response.SponsorPlan.Type.GOLD -> SponsorPlan.Type.Gold
-                    io.github.droidkaigi.confsched2018.data.api.response.SponsorPlan.Type.SILVER -> SponsorPlan.Type.Silver
-                    io.github.droidkaigi.confsched2018.data.api.response.SponsorPlan.Type.SUPPORTER -> SponsorPlan.Type.Supporter
-                    io.github.droidkaigi.confsched2018.data.api.response.SponsorPlan.Type.TECHNICAL_FOR_NETWORK -> SponsorPlan.Type.TechnicalForNetwork
+                    ResponseSponsorType.PLATINUM -> SponsorPlan.Type.Platinum
+                    ResponseSponsorType.GOLD -> SponsorPlan.Type.Gold
+                    ResponseSponsorType.SILVER -> SponsorPlan.Type.Silver
+                    ResponseSponsorType.SUPPORTER -> SponsorPlan.Type.Supporter
+                    ResponseSponsorType.TECHNICAL_FOR_NETWORK -> SponsorPlan.Type.TechnicalForNetwork
                 }, it.groups.map {
                     SponsorGroup(it.sponsors.map {
-                        if (it.base64Img == it.imgUrl) {
-                            Timber.d(it.link)
+                        val (base64Img, imgUrl) = it.base64Img to it.imgUrl
+
+                        val imageUri = when {
+                            base64Img != null -> Sponsor.ImageUri.Base64ImageUri(base64Img)
+                            imgUrl != null -> Sponsor.ImageUri.NetworkImageUri(imgUrl)
+                            else -> throw IllegalStateException("a json file is broken.")
                         }
-                        Sponsor(it.link, if (it.base64Img == null) Sponsor.ImageUri.NetworkImageUri(it.imgUrl!!) else Sponsor.ImageUri.Base64ImageUri(it.base64Img!!))
+
+                        Sponsor(it.link, imageUri)
                     })
                 })
             }
-        }
+        }.toFlowable()
 }
