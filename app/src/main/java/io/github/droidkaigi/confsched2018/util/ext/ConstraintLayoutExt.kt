bc992cf1b0c4cf696b5f60960a1c428718d02f4a
Rui Kowase <rui.kowase@gmail.com>
2018-01-14 12:32:22 +0900

    Delete unused extension

--- app/src/main/java/io/github/droidkaigi/confsched2018/util/ext/ConstraintLayoutExt.kt
+++
@@ -1,6 +0,0 @@
-package io.github.droidkaigi.confsched2018.util.ext
-
-import android.support.constraint.ConstraintLayout
-import android.support.constraint.ConstraintSet
-
-fun ConstraintLayout.clone(): ConstraintSet = ConstraintSet().apply { clone(this) }
