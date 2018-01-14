2d99be103658e7bba99b2c3da46b709bc5c11950
Jumpei Matsuda <j.matsuda.bb26th@gmail.com>
2018-01-15 02:33:45 +0900

    Removed unused code

--- app/src/main/java/io/github/droidkaigi/confsched2018/data/repository/SponsorPlanDataRepository.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/data/repository/SponsorPlanDataRepository.kt
@@ -6,7 +6,6 @@ import io.github.droidkaigi.confsched2018.model.Lang
 import io.github.droidkaigi.confsched2018.model.SponsorPlan
 import io.reactivex.Flowable
 import javax.inject.Inject
-import io.github.droidkaigi.confsched2018.data.api.response.SponsorPlan.Type as ResponseSponsorType
 
 class SponsorPlanDataRepository @Inject constructor(
         private val sponsorApi: SponsorApi
--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/sponsors/SponsorsViewModel.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/sponsors/SponsorsViewModel.kt
@@ -9,7 +9,6 @@ import io.github.droidkaigi.confsched2018.presentation.common.mapper.toResult
 import io.github.droidkaigi.confsched2018.util.ext.toLiveData
 import io.github.droidkaigi.confsched2018.util.rx.SchedulerProvider
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposables
 import javax.inject.Inject
 
 class SponsorsViewModel @Inject constructor(
@@ -20,9 +19,6 @@ class SponsorsViewModel @Inject constructor(
 
     val sponsors: LiveData<Result<List<SponsorPlan>>> by lazy {
         sponsorPlanRepository.sponsorPlans()
-                .doOnSubscribe {
-                    compositeDisposable.add(Disposables.fromSubscription(it))
-                }
                 .toResult(schedulerProvider)
                 .toLiveData()
     }
