2e89434433f332a089c7fa576773c5594ade0fbe
futabooo <takahiro.futagawa@gmail.com>
2018-01-16 05:15:47 +0900

    Add SessionFeedback Table

--- app/src/main/java/io/github/droidkaigi/confsched2018/data/db/SessionDatabase.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/data/db/SessionDatabase.kt
@@ -2,15 +2,19 @@ package io.github.droidkaigi.confsched2018.data.db
 
 import io.github.droidkaigi.confsched2018.data.api.response.Response
 import io.github.droidkaigi.confsched2018.data.db.entity.RoomEntity
+import io.github.droidkaigi.confsched2018.data.db.entity.SessionFeedbackEntity
 import io.github.droidkaigi.confsched2018.data.db.entity.SessionWithSpeakers
 import io.github.droidkaigi.confsched2018.data.db.entity.SpeakerEntity
 import io.github.droidkaigi.confsched2018.data.db.entity.TopicEntity
+import io.github.droidkaigi.confsched2018.model.SessionFeedback
 import io.reactivex.Flowable
 
 interface SessionDatabase {
     fun getAllSessions(): Flowable<List<SessionWithSpeakers>>
+    fun getAllSessionFeedback(): Flowable<List<SessionFeedbackEntity>>
     fun getAllSpeaker(): Flowable<List<SpeakerEntity>>
     fun getAllRoom(): Flowable<List<RoomEntity>>
     fun getAllTopic(): Flowable<List<TopicEntity>>
     fun save(response: Response)
+    fun saveSessionFeedback(sessionFeedback: SessionFeedback)
 }
