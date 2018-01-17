9c2552dc31e25a38bad077391d2ec513c646940d
Nobuya  Oshiro <nobuya.oshiro@gmail.com>
2018-01-18 01:05:35 +0900

    add map screen

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/map/MapFragment.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/map/MapFragment.kt
@@ -6,18 +6,70 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
 import io.github.droidkaigi.confsched2018.databinding.FragmentMapBinding
 import io.github.droidkaigi.confsched2018.di.Injectable
 import javax.inject.Inject
 
-class MapFragment : Fragment(), Injectable {
+class MapFragment : Fragment(), Injectable, OnMapReadyCallback {
+
     private lateinit var binding: FragmentMapBinding
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private var mapView: MapView? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding = FragmentMapBinding.inflate(inflater, container!!, false)
+
+        with(binding.mapView) {
+            mapView = this
+            onCreate(savedInstanceState)
+            getMapAsync(this@MapFragment)
+        }
+
         return binding.root
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        // TODO
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
     }
 
     companion object {
