144922178094da65293d453310d1687a5ad879d5
MihoSasaki <miho.sprit@gmail.com>
2018-01-18 10:52:28 +0900

    add logic for showing layout only after session finishes #141

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/sessions/item/SpeechSessionItem.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/sessions/item/SpeechSessionItem.kt
@@ -6,6 +6,9 @@ import io.github.droidkaigi.confsched2018.R
 import io.github.droidkaigi.confsched2018.databinding.ItemSpeechSessionBinding
 import io.github.droidkaigi.confsched2018.model.Session
 import io.github.droidkaigi.confsched2018.util.lang
+import java.util.Calendar
+import java.util.Date
+import java.util.TimeZone
 
 data class SpeechSessionItem(
         override val session: Session.SpeechSession,
@@ -28,7 +31,18 @@ data class SpeechSessionItem(
         }
         viewBinding.isShowDayNumber = isShowDayNumber
         viewBinding.simplify = simplify
+        viewBinding.isFinished = isFinishedSession(session)
+        viewBinding.goToQuestionnaire.setOnClickListener {
+            //TODO: will implement this. Please check comments of issue #141
+        }
     }
 
     override fun getLayout(): Int = R.layout.item_speech_session
+
+    fun isFinishedSession(session: Session.SpeechSession): Boolean {
+        val cal: Calendar = Calendar.getInstance(TimeZone.getTimeZone("JST"));
+        cal.setTimeInMillis(System.currentTimeMillis())
+        val now: Date = cal.getTime()
+        return now.time.toInt() > session.endTime.getTime().toInt()
+    }
 }
