be47cea42dae6febee7a7eacf67482d3358ec6b6
Jumpei Matsuda <j.matsuda.bb26th@gmail.com>
2018-01-18 13:33:47 +0900

    Renamed a weird method name

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/binding/ConstraintBinding.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/binding/ConstraintBinding.kt
@@ -6,7 +6,7 @@ import android.support.constraint.ConstraintSet
 import android.view.View
 
 @BindingAdapter("layout_constraintDimensionRatio")
-fun View.layout_constraintDimensionRatio(newDimensionRatio: String) {
+fun View.setConstraintDimensionRatio(newDimensionRatio: String) {
     val id = id.takeIf { it != View.NO_ID } ?: return
     val parent = parent as? ConstraintLayout ?: return
     val lp = layoutParams as? ConstraintLayout.LayoutParams ?: return
