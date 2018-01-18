196730e3b7df8cc33fd8f62f061c1f7c3af8e2d3
鈴木 白斗 <hakutosuzuki@crooz.co.jp>
2018-01-18 13:10:24 +0900

    add CoordinatorLayout

---
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/view/BottomBarBehavior.kt
@@ -0,0 +1,33 @@
+package io.github.droidkaigi.confsched2018.presentation.common.view
+
+import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.support.design.widget.AppBarLayout
+import android.support.design.widget.CoordinatorLayout
+import android.support.v4.view.ViewCompat
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * A custom view for showing move next or prev session
+ *
+ * ref: https://github.com/DroidKaigi/conference-app-2018/issues/180
+ * Created by hakutosuzuki on 2018/01/18.
+ */
+
+class BottomBarBehavior(context: Context, attrs: AttributeSet) : CoordinatorLayout.Behavior<ConstraintLayout>(context, attrs) {
+
+    private var defaultDependencyTop = -1
+
+    override fun layoutDependsOn(parent: CoordinatorLayout?, bottomBar: ConstraintLayout?, dependency: View?): Boolean {
+        return dependency is AppBarLayout
+    }
+
+    override fun onDependentViewChanged(parent: CoordinatorLayout?, bottomBar: ConstraintLayout?, dependency: View?): Boolean {
+        if (defaultDependencyTop == -1) {
+            defaultDependencyTop = dependency!!.top
+        }
+        ViewCompat.setTranslationY(bottomBar!!, (-dependency!!.top + defaultDependencyTop).toFloat())
+        return true
+    }
+}
