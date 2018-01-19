6776faa66da7ddd692c6f2940c19eb6e7efccf9e
neonankiti <yk.nanri@gmail.com>
2018-01-19 01:19:29 -0800

    create AboutThisApp model

---
+++ model/src/main/java/io/github/droidkaigi/confsched2018/model/AboutThisApp.kt
@@ -0,0 +1,8 @@
+package io.github.droidkaigi.confsched2018.model
+
+data class AboutThisApp(
+        val id: Int,
+        val name: Int,
+        val description: Int,
+        val navigationUrl: String?
+)
