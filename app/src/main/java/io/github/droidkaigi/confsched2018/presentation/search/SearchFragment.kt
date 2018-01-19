05c2870cc0074e38c9dd1a2499573d15be0fb632
midnight.walker <from.midnight.walker@gmail.com>
2018-01-20 01:31:40 +0900

    added space after if and changed variable name to more meaningful

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/search/SearchFragment.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/search/SearchFragment.kt
@@ -156,8 +156,8 @@ class SearchFragment : Fragment(), Injectable {
         })
         changeSearchViewTextColor(searchView)
 
-        searchView.setOnQueryTextFocusChangeListener { view, b ->
-            if(!b) {
+        searchView.setOnQueryTextFocusChangeListener { view, hasFocus ->
+            if (!hasFocus) {
                 val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as
                         InputMethodManager
                 imm.hideSoftInputFromWindow(view.windowToken, 0)
