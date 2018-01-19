2d9bafd9eb95305e6822432f581da8a2b3e0d9db
takahirom <takam.dev@gmail.com>
2018-01-19 18:16:32 +0900

    Use same preference file kotpref and PreferenceFragmentCompat

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/pref/Prefs.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/pref/Prefs.kt
@@ -4,6 +4,7 @@ import com.chibatching.kotpref.KotprefModel
 import io.github.droidkaigi.confsched2018.R
 
 object Prefs : KotprefModel() {
+    public override val kotprefName: String = "droidkaigi_prefs"
     var enableLocalTime: Boolean by booleanPref(false, key = R.string.pref_key_enable_local_time)
     var enableNotification: Boolean by booleanPref(true, key = R.string.pref_key_enable_notification)
 }
