340f80e6b6e8c5b6f480918d5dc62013ae435fad
takahirom <takam.dev@gmail.com>
2018-01-19 18:36:58 +0900

    Use resource for default value

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/pref/Prefs.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/pref/Prefs.kt
@@ -1,10 +1,16 @@
 package io.github.droidkaigi.confsched2018.presentation.common.pref
 
 import com.chibatching.kotpref.KotprefModel
-import io.github.droidkaigi.confsched2018.R
+import io.github.droidkaigi.confsched2018.util.ext.bool
 
 object Prefs : KotprefModel() {
     public override val kotprefName: String = "droidkaigi_prefs"
-    var enableLocalTime: Boolean by booleanPref(false, R.string.pref_key_enable_local_time)
-    var enableNotification: Boolean by booleanPref(true, R.string.pref_key_enable_notification)
+    var enableLocalTime: Boolean by booleanPref(
+            context.bool(R.bool.pref_default_value_enable_local_time),
+            R.string.pref_key_enable_local_time
+    )
+    var enableNotification: Boolean by booleanPref(
+            context.bool(R.bool.pref_default_value_enable_notification),
+            R.string.pref_key_enable_notification
+    )
 }
