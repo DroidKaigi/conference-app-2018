fc6fc5ad0afe9a79c321e350b32b2f09a840e9d4
takahirom <takam.dev@gmail.com>
2018-01-19 18:23:50 +0900

    Delete key = for argument

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/pref/Prefs.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/pref/Prefs.kt
@@ -5,6 +5,6 @@ import io.github.droidkaigi.confsched2018.R
 
 object Prefs : KotprefModel() {
     public override val kotprefName: String = "droidkaigi_prefs"
-    var enableLocalTime: Boolean by booleanPref(false, key = R.string.pref_key_enable_local_time)
-    var enableNotification: Boolean by booleanPref(true, key = R.string.pref_key_enable_notification)
+    var enableLocalTime: Boolean by booleanPref(false, R.string.pref_key_enable_local_time)
+    var enableNotification: Boolean by booleanPref(true, R.string.pref_key_enable_notification)
 }
