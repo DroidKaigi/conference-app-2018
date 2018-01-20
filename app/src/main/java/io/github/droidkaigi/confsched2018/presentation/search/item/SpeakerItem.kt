4506029a5f4c03ab597c12333fad5161ae04af32
Kenji Abe <kenji.01.star@gmail.com>
2018-01-21 00:37:24 +0900

    Fix lint error

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/search/item/SpeakerItem.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/search/item/SpeakerItem.kt
@@ -8,7 +8,10 @@ import io.github.droidkaigi.confsched2018.R
 import io.github.droidkaigi.confsched2018.databinding.ItemSpeakerBinding
 import io.github.droidkaigi.confsched2018.model.Speaker
 
-data class SpeakerItem(val speaker: Speaker) : BindableItem<ItemSpeakerBinding>(speaker.id.hashCode().toLong()) {
+data class SpeakerItem(
+        val speaker: Speaker
+) : BindableItem<ItemSpeakerBinding>(speaker.id.hashCode().toLong()) {
+
     override fun createViewHolder(itemView: View): ViewHolder<ItemSpeakerBinding> {
         val viewDataBinding =
                 DataBindingUtil.bind<ItemSpeakerBinding>(itemView)
