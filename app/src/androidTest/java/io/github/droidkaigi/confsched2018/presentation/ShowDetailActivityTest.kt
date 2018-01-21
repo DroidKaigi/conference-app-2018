f58bf68504ec55b53107fc3b6f86e55679ae4dd3
numa08 <n511287@gmail.com>
2018-01-21 13:51:37 +0900

    Define LargeTest
    https://github.com/DroidKaigi/conference-app-2018/pull/260#pullrequestreview-90170995

--- app/src/androidTest/java/io/github/droidkaigi/confsched2018/presentation/ShowDetailActivityTest.kt
+++ app/src/androidTest/java/io/github/droidkaigi/confsched2018/presentation/ShowDetailActivityTest.kt
@@ -9,6 +9,7 @@ import android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.support.v7.widget.RecyclerView
@@ -19,6 +20,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.concurrent.TimeUnit
 
+@LargeTest
 @RunWith(AndroidJUnit4::class)
 class ShowDetailActivityTest {
 
