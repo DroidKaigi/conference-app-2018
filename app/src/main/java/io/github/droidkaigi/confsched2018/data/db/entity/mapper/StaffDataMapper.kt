f7c000d3ee14d71313481e519eec72b2800afc0f
Rui Kowase <rui.kowase@gmail.com>
2018-01-21 19:14:45 +0900

    Add mapper of staff data

---
+++ app/src/main/java/io/github/droidkaigi/confsched2018/data/db/entity/mapper/StaffDataMapper.kt
@@ -0,0 +1,16 @@
+package io.github.droidkaigi.confsched2018.data.db.entity.mapper
+
+import io.github.droidkaigi.confsched2018.data.db.entity.ContributorEntity
+import io.github.droidkaigi.confsched2018.data.db.entity.StaffEntity
+import io.github.droidkaigi.confsched2018.model.Contributor
+import io.github.droidkaigi.confsched2018.model.Staff
+
+fun StaffEntity.toStaff(): Staff = Staff(
+        name = name,
+        avatarUrl = avatarUrl,
+        htmlUrl = htmlUrl
+)
+
+fun List<StaffEntity>.toStaff(): List<Staff> = map {
+    it.toStaff()
+}
