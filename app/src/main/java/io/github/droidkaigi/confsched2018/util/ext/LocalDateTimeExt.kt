499528cc95dda7972e55e52a2b479d31f9e64991
Henry Addo <henry@addhen.org>
2018-01-21 07:52:30 +0100

    Set timezone based on preferences configuration

--- app/src/main/java/io/github/droidkaigi/confsched2018/util/ext/LocalDateTimeExt.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/util/ext/LocalDateTimeExt.kt
@@ -1,7 +1,19 @@
 package io.github.droidkaigi.confsched2018.util.ext
 
+import io.github.droidkaigi.confsched2018.presentation.common.pref.Prefs
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneId
+import java.util.TimeZone
 
-fun LocalDateTime.toUnixMills(): Long = atZone(ZoneId.of("JST", ZoneId.SHORT_IDS))
-        .toInstant().toEpochMilli()
+fun LocalDateTime.toUnixMills(): Long {
+    val defaultTimeZone = TimeZone.getDefault()
+    val timeZone = if (Prefs.enableLocalTime) {
+        toZoneId(defaultTimeZone.id)
+    } else {
+        toZoneId("JST")
+    }
+    return atZone(timeZone).toInstant().toEpochMilli()
+}
+
+private fun toZoneId(id: String, shortIds: Map<String, String> = ZoneId.SHORT_IDS) = ZoneId.of(id,
+        shortIds)
