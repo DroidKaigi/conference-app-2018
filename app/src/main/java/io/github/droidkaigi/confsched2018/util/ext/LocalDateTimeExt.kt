bbeb892c875443e6b4e88b944a1f2c911035bb9c
takahirom <takam.dev@gmail.com>
2018-01-22 21:35:04 +0900

    Fix timezone issue

--- app/src/main/java/io/github/droidkaigi/confsched2018/util/ext/LocalDateTimeExt.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/util/ext/LocalDateTimeExt.kt
@@ -1,17 +1,10 @@
 package io.github.droidkaigi.confsched2018.util.ext
 
-import io.github.droidkaigi.confsched2018.presentation.common.pref.Prefs
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneId
-import java.util.TimeZone
 
 fun LocalDateTime.toUnixMills(): Long {
-    val timeZone = if (Prefs.enableLocalTime) {
-        val defaultTimeZone = TimeZone.getDefault()
-        toZoneId(defaultTimeZone.id)
-    } else {
-        toZoneId("JST")
-    }
+    val timeZone = toZoneId("JST")
     return atZone(timeZone).toInstant().toEpochMilli()
 }
 
