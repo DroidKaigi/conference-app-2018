3fc9d242be0c4ee56cbfebac5f3731de7c74dc39
furusin <furusin.oriver@gmail.com>
2018-01-22 18:21:08 +0900

    change for using SpeakersAdapter

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/view/SpeakersSummaryLayout.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/view/SpeakersSummaryLayout.kt
@@ -2,20 +2,22 @@ package io.github.droidkaigi.confsched2018.presentation.common.view
 
 import android.content.Context
 import android.content.res.TypedArray
+import android.graphics.Color
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
+import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import io.github.droidkaigi.confsched2018.R
+import io.github.droidkaigi.confsched2018.R.attr.textColor
 import io.github.droidkaigi.confsched2018.model.Speaker
-import io.github.droidkaigi.confsched2018.util.CustomGlideApp
-import io.github.droidkaigi.confsched2018.util.ext.toGone
-import io.github.droidkaigi.confsched2018.util.ext.toVisible
+import io.github.droidkaigi.confsched2018.presentation.speaker.SpeakerDetailActivity
+
 
 /**
  * A custom view for showing the avatar icons and names of speakers.
@@ -27,35 +29,26 @@ class SpeakersSummaryLayout @JvmOverloads constructor(
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0) : LinearLayout(context, attrs, defStyleAttr) {
 
-    data class CustomAttributes(val maxIcons: Int, val imageSize: Int, val textColor: Int)
+    data class CustomAttributes(val textColor: Int)
 
     private val speakerList: ArrayList<Speaker> = ArrayList()
+    private val speakerContainer: RecyclerView
     private val customAttributes: CustomAttributes
-    private val imageContainer: FrameLayout
-    private val textView: TextView
 
     private fun customAttributesFrom(context: Context, attrs: AttributeSet?): CustomAttributes {
         val res = context.resources
         val defaultAttributes = CustomAttributes(
-                maxIcons = res.getInteger(R.integer.default_speakers_summary_layout_max_icon_count),
-                imageSize = res.getDimensionPixelSize(R.dimen.speaker_image),
                 textColor = 0
         )
 
         return attrs?.let {
             val a: TypedArray = context.theme.obtainStyledAttributes(attrs,
                     R.styleable.SpeakersSummaryLayout, 0, 0)
-            val maxIcons = a.getInt(R.styleable.SpeakersSummaryLayout_max_icon_count,
-                    defaultAttributes.maxIcons)
-            val imageSize = a.getDimensionPixelSize(R.styleable.SpeakersSummaryLayout_image_size,
-                    defaultAttributes.imageSize)
             val textColor = a.getColor(R.styleable.SpeakersSummaryLayout_textColor,
                     defaultAttributes.textColor)
             a.recycle()
 
             return CustomAttributes(
-                    maxIcons = maxIcons,
-                    imageSize = imageSize,
                     textColor = textColor
             )
         } ?: defaultAttributes
@@ -67,11 +60,10 @@ class SpeakersSummaryLayout @JvmOverloads constructor(
         gravity = Gravity.CENTER_VERTICAL
 
         LayoutInflater.from(context).inflate(R.layout.view_spearkers_summary_layout, this)
-        imageContainer = findViewById(R.id.speaker_image_container)
-        textView = findViewById(R.id.speakers)
+        speakerContainer = findViewById(R.id.speaker_container)
 
-        updateIcons()
-        updateText()
+        speakerContainer.setLayoutManager(LinearLayoutManager(context))
+        setSpeakers(speakerList)
     }
 
     /**
@@ -86,59 +78,24 @@ class SpeakersSummaryLayout @JvmOverloads constructor(
      *     app:speakers="@{speakers}"
      *     />
      */
-    fun setSpeakers(speakers: List<Speaker>?) {
-        speakers?.let {
-            speakerList.clear()
-            speakerList.addAll(speakers)
+    fun setSpeakers(speakers: List<Speaker>) {
+        speakerList.clear()
+        speakerList.addAll(speakers)
 
-            updateIcons()
-            updateText()
-        }
+        updateSpearkers()
     }
 
-    private fun updateIcons() {
+    private fun updateSpearkers() {
         if (speakerList.isEmpty()) {
-            imageContainer.toGone()
         } else {
-            imageContainer.removeAllViews()
-            speakerList.take(customAttributes.maxIcons).let { speakers ->
-                val size = speakers.size
-                speakers.reversed().forEachIndexed { index, speaker ->
-                    imageContainer.addView(createIconImageView(speaker, size - 1 - index))
+            var speakerAdapter = SpeakersAdapter(context, speakerList, customAttributes.textColor)
+            speakerContainer.adapter = speakerAdapter
+
+            speakerAdapter.setOnItemClickListener(object : SpeakersAdapter.OnItemClickListener {
+                override fun onClick(view: View, speakerId: String) {
+                    SpeakerDetailActivity.start(context, speakerId)
                 }
-            }
-            imageContainer.toVisible()
-        }
-    }
-
-    private fun createIconImageView(speaker: Speaker, index: Int): View {
-        return ImageView(context).also { imageView ->
-            val imageSize = customAttributes.imageSize
-            imageView.contentDescription =
-                    context.getString(R.string.session_content_description_speaker)
-            imageView.layoutParams = FrameLayout.LayoutParams(imageSize, imageSize).also { lp ->
-                lp.marginStart = index * imageSize * 3 / 4
-            }
-            CustomGlideApp
-                    .with(this)
-                    .load(speaker.imageUrl)
-                    .placeholder(R.drawable.ic_person_black_24dp)
-                    .override(imageSize, imageSize)
-                    .dontAnimate()
-                    .transform(CircleCrop())
-                    .into(imageView)
-        }
-    }
-
-    private fun updateText() {
-        if (speakerList.isEmpty()) {
-            textView.toGone()
-        } else {
-            textView.text = speakerList.joinToString { it.name }
-            if (customAttributes.textColor != 0) {
-                textView.setTextColor(customAttributes.textColor)
-            }
-            textView.toVisible()
+            })
         }
     }
 }
