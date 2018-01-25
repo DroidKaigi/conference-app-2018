3af43949371d0220a21414b016731f10ddd3a3a7
Gen <archiherewego@gmail.com>
2018-01-25 16:51:03 +0900

    Show BNV when content can not be scrolled

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/view/BottomNavigationHideBehavior.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/view/BottomNavigationHideBehavior.kt
@@ -17,6 +17,8 @@ class BottomNavigationHideBehavior : BottomNavigationBehavior {
     @Keep constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
     private var isAnimation: Boolean = false
+    private var isAcceptNestedScroll: Boolean = false
+    private var isProcessedScroll: Boolean = false
     private val animationListener = object : Animator.AnimatorListener {
         override fun onAnimationRepeat(animation: Animator?) {
         }
@@ -37,21 +39,44 @@ class BottomNavigationHideBehavior : BottomNavigationBehavior {
     override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout,
                                      child: BottomNavigationView, directTargetChild: View,
                                      target: View, axes: Int, type: Int): Boolean {
-        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL && type == ViewCompat.TYPE_TOUCH
+    }
+
+    override fun onNestedScrollAccepted(coordinatorLayout: CoordinatorLayout,
+                                        child: BottomNavigationView, directTargetChild: View,
+                                        target: View, axes: Int, type: Int) {
+        isAcceptNestedScroll = true
+        super.onNestedScrollAccepted(coordinatorLayout, child, directTargetChild, target, axes, type)
     }
 
     override fun onNestedScroll(coordinatorLayout: CoordinatorLayout, child: BottomNavigationView,
                                 target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int,
                                 dyUnconsumed: Int, type: Int) {
+        isProcessedScroll = true
         when {
         // positive value: finger's move = touch -> move up (contents are scrolled downward)
             dyConsumed > THRESHOLD_PX -> hideBottomNavigationView(child)
         // negative value: finger's move = touch -> move down (contents are scrolled upward)
             dyConsumed < THRESHOLD_PX -> showBottomNavigationView(child)
+        // ignore case: user scrolls contents upward but contents are too short to scroll
+            dyConsumed == 0 && dyUnconsumed < 0 -> isProcessedScroll = false
             else -> Unit
         }
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed,
                 dxUnconsumed, dyUnconsumed, type)
+    }
+
+    override fun onStopNestedScroll(coordinatorLayout: CoordinatorLayout,
+                                    child: BottomNavigationView, target: View, type: Int) {
+        if (isAcceptNestedScroll && !isProcessedScroll) {
+            // user touches screen but contents are not scrolled
+            // this happens like contents are shot enough which has no reason to scroll
+            // on the situation, it is necessary for the user to show BNV
+            showBottomNavigationView(child)
+        }
+        isAcceptNestedScroll = false
+        isProcessedScroll = false
+        super.onStopNestedScroll(coordinatorLayout, child, target, type)
     }
 
     private fun showBottomNavigationView(view: BottomNavigationView) {
