5ee3ef090881e873050a70fe8cca2aab17e118bd
NoriyoshiSatou <noriyoshi.sato@rootage.co.jp>
2018-01-28 16:43:58 +0900

    delete import

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/detail/SessionDetailFragment.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/detail/SessionDetailFragment.kt
@@ -5,7 +5,6 @@ import android.arch.lifecycle.ViewModelProviders
 import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
