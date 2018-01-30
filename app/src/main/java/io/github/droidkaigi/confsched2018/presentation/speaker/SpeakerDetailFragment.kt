b70a9cab20699a7c6074c9cd991daebd1462c740
k-kiuchi <minismahn@yahoo.co.jp>
2018-01-31 00:17:16 +0900

    fix:issue515 I want to be able to go to GitHub and Twitter and etc.. of the speaker in SpeakerDetailFragment .

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/speaker/SpeakerDetailFragment.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/speaker/SpeakerDetailFragment.kt
@@ -3,6 +3,7 @@ package io.github.droidkaigi.confsched2018.presentation.speaker
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.support.design.widget.BottomSheetDialog
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
@@ -11,6 +12,7 @@ import android.view.ViewGroup
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
 import io.github.droidkaigi.confsched2018.R
+import io.github.droidkaigi.confsched2018.databinding.BottomSheetDialogSpeakerSnsBinding
 import io.github.droidkaigi.confsched2018.databinding.FragmentSpeakerDetailBinding
 import io.github.droidkaigi.confsched2018.di.Injectable
 import io.github.droidkaigi.confsched2018.model.Session
@@ -26,6 +28,7 @@ import javax.inject.Inject
 
 class SpeakerDetailFragment : Fragment(), Injectable {
     private lateinit var binding: FragmentSpeakerDetailBinding
+    private lateinit var dialogBinding: BottomSheetDialogSpeakerSnsBinding
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private val sessionsSection = SimpleSessionsSection()
@@ -49,6 +52,11 @@ class SpeakerDetailFragment : Fragment(), Injectable {
                         container!!,
                         false
                 )
+        dialogBinding =
+                BottomSheetDialogSpeakerSnsBinding.inflate(
+                        inflater,
+                        container!!,
+                        false)
         return binding.root
     }
 
@@ -61,6 +69,7 @@ class SpeakerDetailFragment : Fragment(), Injectable {
                 is Result.Success -> {
                     val speaker = result.data.first
                     binding.speaker = speaker
+                    dialogBinding.speaker = speaker
                     sessionsSection.updateSessions(result.data.second,
                             onFavoriteClickListener,
                             userIdInDetail = speaker.id)
@@ -70,6 +79,36 @@ class SpeakerDetailFragment : Fragment(), Injectable {
                 }
             }
         })
+
+        val dialog = BottomSheetDialog(context!!).apply {
+            setContentView(dialogBinding.root)
+            setCancelable(true)
+        }
+
+        binding.speakerImage.setOnClickListener {
+            dialog.show()
+        }
+        dialogBinding.speakerDetailTwitter.setOnClickListener {
+            dialogBinding.speaker?.twitterUrl ?: return@setOnClickListener
+            launchSNSLink(dialogBinding.speaker!!.twitterUrl!!, dialog)
+        }
+        dialogBinding.speakerDetailGithub.setOnClickListener {
+            dialogBinding.speaker?.githubUrl ?: return@setOnClickListener
+            launchSNSLink(dialogBinding.speaker!!.githubUrl!!, dialog)
+        }
+        dialogBinding.speakerDetailCompany.setOnClickListener {
+            dialogBinding.speaker?.companyUrl ?: return@setOnClickListener
+            launchSNSLink(dialogBinding.speaker!!.companyUrl!!, dialog)
+        }
+        dialogBinding.speakerDetailBlog.setOnClickListener {
+            dialogBinding.speaker?.blogUrl ?: return@setOnClickListener
+            launchSNSLink(dialogBinding.speaker!!.blogUrl!!, dialog)
+        }
+    }
+
+    private fun launchSNSLink(url:String, dialog: BottomSheetDialog) {
+        navigationController.navigateToExternalBrowser(url)
+        dialog.dismiss()
     }
 
     private fun setupRecyclerView() {
