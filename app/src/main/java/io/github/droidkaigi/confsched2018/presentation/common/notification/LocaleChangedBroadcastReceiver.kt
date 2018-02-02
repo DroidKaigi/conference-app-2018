a9387186221aa5b34bece0f512ac62b0bc824c9b
satorufujiwara <holly.wist@gmail.com>
2018-02-02 09:38:47 +0900

    Use top-level function instead of object class.

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/notification/LocaleChangedBroadcastReceiver.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/common/notification/LocaleChangedBroadcastReceiver.kt
@@ -6,10 +6,10 @@ import android.content.Intent
 import timber.log.Timber
 
 class LocaleChangedBroadcastReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent?) {
         Timber.d("LocaleChangedBroadcastReceiver.onReceive")
         if (Intent.ACTION_LOCALE_CHANGED == intent?.action) {
-            NotificationHelper.initNotificationChannel(context!!)
+            context.initNotificationChannel()
         }
     }
 }
