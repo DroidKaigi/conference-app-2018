81695dbf454fe9d464fcd999cb0df1f192d55b13
Masatoshi Kubode <swi.masatoshi@gmail.com>
2018-02-04 22:56:09 +0900

    lint: Add line break

--- app/src/main/java/io/github/droidkaigi/confsched2018/util/ext/RecyclerViewExt.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/util/ext/RecyclerViewExt.kt
@@ -7,8 +7,10 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 
 inline fun RecyclerView.addOnScrollListener(
-        crossinline onScrollStateChanged: (recyclerView: RecyclerView?, newState: Int) -> Unit = { _, _ -> },
-        crossinline onScrolled: (recyclerView: RecyclerView?, dx: Int, dy: Int) -> Unit = { _, _, _ -> }
+        crossinline onScrollStateChanged: (recyclerView: RecyclerView?, newState: Int) -> Unit
+        = { _, _ -> },
+        crossinline onScrolled: (recyclerView: RecyclerView?, dx: Int, dy: Int) -> Unit
+        = { _, _, _ -> }
 ) {
     addOnScrollListener(object : RecyclerView.OnScrollListener() {
         override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
