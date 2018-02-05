3ad9cf0ca395b714d7d736d44e650cdaa4e4df87
panpanini <panpanini@mercari.com>
2018-02-05 17:57:11 +0900

    Create SimpleTransitionListener open class
    This should make TransitionListeners easier to use, because we only have
    to implement the methods we care about

---
+++ app/src/main/java/io/github/droidkaigi/confsched2018/util/SimpleTransitionListener.kt
@@ -0,0 +1,20 @@
+package io.github.droidkaigi.confsched2018.util
+
+import android.transition.Transition
+
+open class SimpleTransitionListener: Transition.TransitionListener {
+    override fun onTransitionEnd(transition: Transition?) {
+    }
+
+    override fun onTransitionResume(transition: Transition?) {
+    }
+
+    override fun onTransitionPause(transition: Transition?) {
+    }
+
+    override fun onTransitionCancel(transition: Transition?) {
+    }
+
+    override fun onTransitionStart(transition: Transition?) {
+    }
+}
