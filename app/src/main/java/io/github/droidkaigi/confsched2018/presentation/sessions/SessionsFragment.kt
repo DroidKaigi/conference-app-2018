06c5ec02a5cb90f908eea67c855b5990dbbfd84b
takahirom <takam.dev@gmail.com>
2018-02-06 00:48:33 +0900

    Hotfix for changing tab crash

--- app/src/main/java/io/github/droidkaigi/confsched2018/presentation/sessions/SessionsFragment.kt
+++ app/src/main/java/io/github/droidkaigi/confsched2018/presentation/sessions/SessionsFragment.kt
@@ -166,6 +166,8 @@ class SessionsFragment : Fragment(), Injectable, Findable, OnReselectedListener 
 
     private fun saveCurrentSession() {
         val currentItem = binding.sessionsViewPager.currentItem
+        if (sessionsViewPagerAdapter.count <= currentItem) return
+
         PreviousSessionPrefs.previousSessionTabIndex = currentItem
         PreviousSessionPrefs.previousSessionTabMode = sessionsViewModel.tabMode
         val fragment = sessionsViewPagerAdapter
@@ -179,6 +181,8 @@ class SessionsFragment : Fragment(), Injectable, Findable, OnReselectedListener 
         when (sessionsViewModel.tabMode) {
             SessionTabMode.ROOM -> {
                 val currentItem = binding.sessionsViewPager.currentItem
+                if (sessionsViewPagerAdapter.count <= currentItem) return
+
                 val fragment = sessionsViewPagerAdapter
                         .instantiateItem(binding.sessionsViewPager, currentItem)
 
@@ -196,7 +200,7 @@ class SessionsFragment : Fragment(), Injectable, Findable, OnReselectedListener 
     private fun reopenPreviousOpenedItem() {
         val previousItem = PreviousSessionPrefs.previousSessionTabIndex
         if (previousItem < 0) return
-        if (sessionsViewPagerAdapter.count < previousItem) return
+        if (sessionsViewPagerAdapter.count <= previousItem) return
 
         binding.sessionsViewPager.currentItem = previousItem
         val fragment = sessionsViewPagerAdapter
